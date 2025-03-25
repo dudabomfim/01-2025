ede05b36be73048ea0e613223290c91fc8e8b70f
dudabomfim <159458716+dudabomfim@users.noreply.github.com>
2025-03-25 10:50:13 -0300

    Add files via upload

---
+++ 01-2025-main/Eduarda Bomfim/site-acessivel-projeto-inicial/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded',function () {
+    const aumentaFonteBotao = document.getElementById ('aunaentar-fonte');
+
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener ('click', function () {
+        tamanhoAtualFonte +=;
+    })
+})
